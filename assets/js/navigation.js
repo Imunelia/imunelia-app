92fb4e8ecab0012d80b032c309a26d9551b4bdc1
vct5d8tb75-sketch <vct5d8tb75@privaterelay.appleid.com>
2026-07-11 12:20:39 +0200

    Align ULTRA pack with product grid

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -1,4 +1,17 @@
 (() => {
+  const ultraPackImages = document.querySelectorAll('.product-pack-photo--ultra');
+
+  ultraPackImages.forEach((image) => {
+    image.setAttribute('src', 'assets/immunalia-ultra-product.svg?v=20260711-aligned1');
+    image.setAttribute('width', '1006');
+    image.setAttribute('height', '800');
+    image.classList.remove('product-pack-photo--ultra');
+
+    if (!image.classList.contains('product-pack-photo--ultra-large')) {
+      image.closest('.ultra-photo-stage')?.classList.remove('ultra-photo-stage');
+    }
+  });
+
   const header = document.querySelector('[data-site-header]');
   const toggle = document.querySelector('[data-nav-toggle]');
   const nav = document.getElementById('site-nav');
